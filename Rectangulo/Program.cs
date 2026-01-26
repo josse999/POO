6d203f85e75f8a778bd6349c529ae72f3849fa04
josse999 <joseantoniomagallanesnovoa@gmail.com>
2026-01-26 16:39:12 -0600

    Rectangulo

---
+++ Rectangulo/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int numOne, numTwo, result;
+
+Console.WriteLine("Ingrese la base del rectangulo: ");
+numOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del rectangulo: ");
+numTwo = int.Parse(Console.ReadLine());
+
+result = numOne * numTwo;
+
+Console.WriteLine("El area del rectangulo es: "+result);
+Console.WriteLine("Fin.");
+
+
